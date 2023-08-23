a0a62f5861087f33b1240525bbf076106e952601
Gabriel Aborhey Fleischer <113002881+drgabelive@users.noreply.github.com>
2023-08-23 15:22:18 +0000

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,16 @@ import { ThemeContext } from "../../context/themeContext";
 import Search from "../../components/Search/Search";
 import Hero from "../../components/Hero/Hero";
 
+// Create the functional component Home
 const Home = () => {
+  // Use the theme context to access the current theme
   const { theme } = useContext(ThemeContext);
+
+  // Define theme styles for light and dark themes
   const LightTheme = { backgroundColor: "#fff", color: "#000" };
   const DarkTheme = { backgroundColor: "#202C36", color: "#fff" };
+
+  // Render the component
   return (
     <div style={theme === "light" ? LightTheme : DarkTheme}>
       <Search theme={theme} />
