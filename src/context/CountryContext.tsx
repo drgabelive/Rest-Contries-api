b84592791d5a755ececf190e64252f018fa08100
Gabriel Aborhey Fleischer <113002881+drgabelive@users.noreply.github.com>
2023-08-23 15:33:44 +0000

    Update CountryContext.tsx

--- src/context/CountryContext.tsx
+++ src/context/CountryContext.tsx
@@ -1,18 +1,24 @@
 import { createContext } from "react";
 
+// Define the interface for the country data
 interface Country {
-   name: { official: string };
-   flags: {png: string, alt: string};
-   population: number;
-   region: string;
-   capital: string;
- }
- interface CountryContextType{
-   countries: Country[];
-  setCountries: (countries: Country[]) => void;
- }
+  name: { official: string };
+  flags: { png: string; alt: string };
+  population: number;
+  region: string;
+  capital: string;
+}
 
+// Define the interface for the CountryContextType
+interface CountryContextType {
+  countries: Country[];
+  setCountries: (countries: Country[]) => void;
+}
+
+// Create a context using createContext
 export const CountryContext = createContext<CountryContextType>({
   countries: [],
-  setCountries: ()=>{console.log("hi")}, 
+  setCountries: () => {
+    console.log("hi");
+  },
 });
