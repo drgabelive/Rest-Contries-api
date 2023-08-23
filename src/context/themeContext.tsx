1273d45137f808bf5cb08abdb7af4b392e67becd
Gabriel Aborhey Fleischer <113002881+drgabelive@users.noreply.github.com>
2023-08-23 15:34:49 +0000

    Update themeContext.tsx

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -1,10 +1,12 @@
 import { createContext } from "react";
 
+// Define the interface for the ThemeContextType
 interface ThemeContextType {
   theme: string;
   setTheme: (theme: string) => void;
 }
 
+// Create a context using createContext
 export const ThemeContext = createContext<ThemeContextType>({
   theme: "light",
   setTheme: () => {
